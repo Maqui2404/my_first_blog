d90331dd4e6b61f463c8b11b473d2543a1a2b7e8
MarcoF <marfimaqui@gmail.com>
2022-10-14 21:36:46 -0500

    oficial

--- js/javascript.js
+++ js/javascript.js
@@ -22,19 +22,19 @@ function ver_mas1() {
   document.getElementById("ven").style.display = "block";
 }
 
-function sendEmail() {
-  Email.send({
-    Host: "smtp.gmail.com",
-    Username: "sender@email_address.com",
-    Password: "Enter your password",
-    To: "receiver@email_address.com",
-    From: "sender@email_address.com",
-    Subject: "Sending Email using javascript",
-    Body: "Well that was easy!!",
-  }).then(function (message) {
-    alert("mail sent successfully");
-  });
-}
+// function sendEmail() {
+//   Email.send({
+//     Host: "smtp.gmail.com",
+//     Username: "sender@email_address.com",
+//     Password: "Enter your password",
+//     To: "receiver@email_address.com",
+//     From: "sender@email_address.com",
+//     Subject: "Sending Email using javascript",
+//     Body: "Well that was easy!!",
+//   }).then(function (message) {
+//     alert("mail sent successfully");
+//   });
+// }
 
 
 document.addEventListener("keyup", (e) => {
